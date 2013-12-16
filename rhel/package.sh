78a0ab881148f533c2df89862ea5d257c32db3b5
Dave Cahill <dcahill@midokura.com>
2013-12-16 17:59:41 +0900

    Don't rely on user being midokura
    package.sh for RHEL was assuming /home/midokura existed

--- rhel/package.sh
+++ rhel/package.sh
@@ -3,6 +3,6 @@
 version=`grep ^Version rhel/*.spec | cut -d ' ' -f 5`
 package=python-midonetclient
 
-git archive HEAD --prefix=$package-$version/ -o /home/midokura/rpmbuild/SOURCES/$package-$version.tar
-gzip /home/midokura/rpmbuild/SOURCES/$package-$version.tar
+git archive HEAD --prefix=$package-$version/ -o ~/rpmbuild/SOURCES/$package-$version.tar
+gzip ~/rpmbuild/SOURCES/$package-$version.tar
 rpmbuild -ba rhel/$package.spec
