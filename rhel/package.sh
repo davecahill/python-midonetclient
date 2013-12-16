d4e9ea6d3b584b30e9d9091e3d841931e1a9bbfe
Dave Cahill <dcahill@midokura.com>
2013-12-16 18:21:42 +0900

    Change rhel/package.sh to take version and release
    - No longer track version number within this repo
    - Calling "package.sh version release" produces
      a package called python-midonetclient-version-release

--- rhel/package.sh
+++ rhel/package.sh
@@ -1,8 +1,24 @@
 #!/bin/bash -e
 
-version=`grep ^Version rhel/*.spec | cut -d ' ' -f 5`
+# Take version and release as parameters
+version=$1
+release=$2
+
 package=python-midonetclient
+specfile=rhel/$package.spec
+specfile_template=$specfile.template
+
+# Create specfile with correct version
+
+# Delete if it exists for some reason
+rm -f $specfile
+
+# Make a copy
+cp $specfile_template $specfile
+sed -i "s/VERSION_PLACEHOLDER/$version/g" $specfile
+sed -i "s/RELEASE_PLACEHOLDER/$release/g" $specfile
 
 git archive HEAD --prefix=$package-$version/ -o ~/rpmbuild/SOURCES/$package-$version.tar
 gzip ~/rpmbuild/SOURCES/$package-$version.tar
-rpmbuild -ba rhel/$package.spec
+rpmbuild -ba $specfile
+rm -f $specfile
